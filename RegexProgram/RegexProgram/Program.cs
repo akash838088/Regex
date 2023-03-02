7b1a321bb1b06113bb2af9517e0bd2cffed64745
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-03-02 23:17:05 +0530

    Add UC2-LastName

--- RegexProgram/RegexProgram/Program.cs
+++ RegexProgram/RegexProgram/Program.cs
@@ -11,7 +11,7 @@ namespace RegexProgram
         static void Main(string[] args)
         {
             RegexPatten regexPatten = new RegexPatten();
-            regexPatten.ValidateFirstName("Akash");
+            regexPatten.ValidateName("Akash", "Girase");
             Console.ReadLine();
 
         }
