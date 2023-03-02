18d2af2bbf2117f45ee8235fbc17ccc5fa88055a
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-03-02 23:09:19 +0530

    Add UC1-ValidFirstName

--- RegexProgram/RegexProgram/Program.cs
+++ RegexProgram/RegexProgram/Program.cs
@@ -11,15 +11,9 @@ namespace RegexProgram
         static void Main(string[] args)
         {
             RegexPatten regexPatten = new RegexPatten();
-            regexPatten.ValidateNameFirstName("Akash");
-            regexPatten.ValidateNameLastName("Girase");
-            regexPatten.ValidateEmail("abc@yahoo.com");
-            regexPatten.ValidateMobileNumber("91 8380885318");
-            regexPatten.ValidatePasswordRoule1("password");
-            regexPatten.ValidatePasswordRoule2("pasSword");
-            regexPatten.ValidatePasswordRoule3("pasSwor1");
-            regexPatten.ValidatePasswordRoule4("1234@akk");
+            regexPatten.ValidateFirstName("Akash");
             Console.ReadLine();
+
         }
     }
 }
