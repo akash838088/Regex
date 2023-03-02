740b5ae860327b0aef849fe7b69bb0b789967613
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-03-03 00:02:27 +0530

    Add UC9-TestCase

--- RegexProgram/RegexProgram/Program.cs
+++ RegexProgram/RegexProgram/Program.cs
@@ -12,15 +12,15 @@ namespace RegexProgram
         static void Main(string[] args)
         {
             RegexPatten regexPatten = new RegexPatten();
-            regexPatten.ValidateName("Akash", "Girase");
-            regexPatten.ValidateEmail("abc.xyz@bl.co.in");
+            regexPatten.ValidateNameFirstName("Akash");
+            regexPatten.ValidateNameLastName("Girase");
+            regexPatten.ValidateEmail("abc@yahoo.com");
             regexPatten.ValidateMobileNumber("91 8380885318");
             regexPatten.ValidatePasswordRoule1("password");
             regexPatten.ValidatePasswordRoule2("pasSword");
-            regexPatten.ValidatePasswordRoule3("pasSword1");
-            regexPatten.ValidatePasswordRoule4("pasSword@_123");
+            regexPatten.ValidatePasswordRoule3("pasSwor1");
+            regexPatten.ValidatePasswordRoule4("1234@aka");
             Console.ReadLine();
-
         }
     }
 }
