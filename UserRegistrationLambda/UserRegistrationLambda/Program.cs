605b08ad9966ff6a07c9e81c7786ec61ea32256e
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-03-10 23:44:40 +0530

    Add UC13-RefractorLambda

---
+++ UserRegistrationLambda/UserRegistrationLambda/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationLambda
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RegistrationLambda registrationLambda = new RegistrationLambda();
+            registrationLambda.CheckFirstName("Akash");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserRegistrationLambda/UserRegistrationLambda/RegistrationModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationLambda
+{
+    public class RegistrationModel
+    {
+        public Regex FirstNamePatten = new Regex(@"^[A-Z][a-z]{3,}?");
+        public Regex LastNamePatten = new Regex(@"^[A-Z][a-z]{3,}?");
+        public Regex EmailPatten = new Regex(@"^[a-zA-Z]+[.+_-]{0,1}[a-z]+[@][a-zA-Z]+[.][a-z]{2,3}([.][a-z]{2}){0,1}$");
+        public Regex MobileNumPatten = new Regex(@"[0-9]{1,2}[ ][0-9]{10}");
+        public Regex Password1Patten = new Regex(@"[A-Z a-z 0-9]{8,}");
+        public Regex Password2Patten = new Regex(@"(?=.*[A-Z])[A-Za-z0-9]{8,}");
+        public Regex Password3Patten = new Regex(@"(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}");
+        public Regex Password4Patten = new Regex(@"""^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!@#$%^&*()_+=-]).{8,}$""");
+    }
+}
